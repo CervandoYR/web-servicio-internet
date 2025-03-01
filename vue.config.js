05bc1709a003c6fe0c8dca510886976739628f3b
CervandoYR <yactayocervando@gmail.com>
2025-03-01 17:58:05 -0500

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/web-servicio-internet/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   devServer: {
     proxy: {
       '/api': {
